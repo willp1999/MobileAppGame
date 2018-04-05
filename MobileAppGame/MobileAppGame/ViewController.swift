bf423c2bd35823bd1f6e01a0044d8bec599dc4c1
Kashyap, Divya <130022@s020rmc148-21.local>
2018-04-05 14:11:45 -0500

    Divya testing

--- MobileAppGame/MobileAppGame/ViewController.swift
+++ MobileAppGame/MobileAppGame/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("testing github")
+        
+        print("Divya")
     }
 
     override func didReceiveMemoryWarning() {
